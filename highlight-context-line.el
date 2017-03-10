2e8ed6561c8e7d699fc818d2a70d345ec1edf0d9
Stefan Kamphausen <stefan.kamphausen@acrolinx.com>
2017-03-10 22:56:31 +0100

    The big 2.0 rewrite (Beta1)
    This is basically a complete rewrite of this library. Hence version
    2.0 (with a careful "beta1" added).
    
    * No more support for other flavors of Emacs than GNU Emacs
    * Turned into a minor mode
    * 14 years after the previous version -- yay!

--- highlight-context-line.el
+++ highlight-context-line.el
@@ -1,18 +1,19 @@
 ;;; highlight-context-line.el --- Highlight last visible line when scrolling
-;; $Id: highlight-context-line.el,v 1.5 2003/01/27 21:42:28 ska Exp $
-;; Copyright (C) 2002-2003 by Stefan Kamphausen, Claus Brunzema
-;; Authors:
-;;           Stefan Kamphausen <mail@skamphausen.de>
-;;           Claus Brunzema    <mail@cbrunzema.de>
-;; Keywords: services, user
-(defvar highlight-context-version "1.5"
-  "Version number of highlight-context-line")
-;; This file is not part of XEmacs.
 
-;; This program is free software; you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
+;; Copyright (C) 2002-2017 by Stefan Kamphausen
+;; Authors: Stefan Kamphausen <www.skamphausen.de>
+;; Created: 2002
+;; Keywords: faces, services, user
+
+(defvar highlight-context-line-version "2.0-beta1"
+  "Version number of highlight-context-line")
+
+;; This file is not part of Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
 
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,139 +21,123 @@
 ;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING. If not, write to the Free
-;; Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-;; 02111-1307, USA.
-
+;; along with this program.  If not, see
+;; <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+
 ;; HighlightContextLine on the Web:
 ;; Main page:
-;; http://www.skamphausen.de/software/skamacs/highlight-context.html
-;; German intro:
-;; http://www.skamphausen.de/xemacs/lisp/highlight-context.html
+;; http://www.github.com/ska2342/highlight-context-line/
 
-;; This piece of code highlights the last visible line when scrolling
+;; This minor mode highlights the last visible line when scrolling
 ;; full windows. It was inspired by the postscript viewer gv which
-;; does a similar thing using a line across the screen
-;; It honours the variable next-screen-context-lines.
-
-;;; TODO:
-;; - it would be nice to draw a horizontal line across the whole frame.
-;;   The approach with using a face-background is just a workaround.
-;; - The usual GNU Emacs compatibility stuff could be done here.
+;; does a similar thing using a line across the screen. It honours the
+;; variable next-screen-context-lines.
 
 ;;; ChangeLog
+;; 2.0   March 2017
+;;       - Total rewrite
+;;       - No more XEmacs support, I am afraid
 ;; 1.5   January  2003
 ;;       - GNU Emacs compatibility due to request of
 ;;         Sami Salkosuo (http://members.fortunecity.com/salkosuo)
 ;;       - untabify file
 ;; 1.3   December 2002
 ;;       CVS, "official" webpage
-;; Written in 2002, the year I finally started writing elisp code
+;;
+;; Written in 2002, the year I finally started writing some serious
+;; elisp
 
 ;;; Code:
-(require 'advice)
 
-;;; User options ---------------------------------------------------------
 (defgroup highlight-context-line nil
   "Highlight last visible line when scrolling."
   :tag "Highlight Context"
-  :link '(url-link :tag "Home Page" 
-                   "http://www.skamphausen.de/software/skamacs/")
+  :link '(url-link
+          :tag "Home Page" 
+          "http://www.github.com/ska2342/highlight-context-line/")
   :link '(emacs-commentary-link
-          :tag "Commentary in highlight-context-line.el" "highlight-context-line.el")
+          :tag "Commentary in highlight-context-line.el"
+          "highlight-context-line.el")
   :prefix "highlight-context-line-"
-  :group 'basics)
+  :group 'convenience)
 
-(defcustom highlight-context-line-background "#bbbbbb"
-  "*The background color to use to highlight the context line"
-  :type 'string
-  :group 'highlight-context-line)
+;; I wanted :underline t, but for some reason that does not underline
+;; to the window edge, while background changes do.
+(defface highlight-context-line-face
+  '((t (:inherit highlight)))
+    "Face used to highlight the context line."
+    :group 'highlight-context-line)
 
-(defvar highlight-context-line-face
-  (copy-face 'default 'highlight-context-line-face)
-  "Face for the highlighting of the context line.")
+(defvar highlight-context-line-overlay
+  ;; init somewhere, will be moved later
+  (make-overlay 1 1)
+  "Overlay to use for highlighting.")
 
-(defvar highlight-context-line-extent nil
-  "Extent to use for highlighting.")
+(overlay-put highlight-context-line-overlay
+             'face 'highlight-context-line-face)
 
-;; Compatibility
-(if (featurep 'xemacs)
-    (progn ;; XEmacs
-      (defalias 'highlight-context-line-make-extent
-        'make-extent)
-      (defalias 'highlight-context-line-set-extent-property
-        'set-extent-property)
-      (defalias 'highlight-context-line-delete-extent
-        'delete-extent)
-      (defalias 'highlight-context-line-height
-        'window-displayed-height))
-  ;; GNU Emacs
-  (defalias 'highlight-context-line-make-extent 'make-overlay)
-  (defalias 'highlight-context-line-set-extent-property 'overlay-put)
-  (defalias 'highlight-context-line-delete-extent 'delete-overlay)
-  (defalias 'highlight-context-line-height 'window-height))
+;;;###autoload
+(define-minor-mode highlight-context-line-mode
+  "Toggle highlighting of context line when scrolling.
+With a prefix argument ARG, enable the mode if ARG is positive,
+and disable it otherwise.  If called from Lisp, enable the mode
+if ARG is omitted or nil.
 
-(when (not (fboundp 'point-at-bol))
-    (progn
-      (defun point-at-bol ()
-        (save-excursion
-          (beginning-of-line)
-          (point)))
-      (defun point-at-eol ()
-        (save-excursion
-          (end-of-line)
-          (point)))))
+When scrolling a buffer up this minor mode highlights the line
+that was at the top of the window before scrolling. When
+scrolling down, the bottommost line of the window at start of
+scrolling is highlighted. The respective line is considered
+the context line."
+  :group 'highlight-context-line
+  (if highlight-context-line-mode
+      (progn
+        (add-hook 'post-command-hook
+                  #'highlight-context-line-highlight)
+        (add-hook 'pre-command-hook
+                  #'highlight-context-line-unhighlight))
+    (remove-hook 'post-command-hook
+              #'highlight-context-line-highlight)
+    (remove-hook 'pre-command-hook
+              #'highlight-context-line-unhighlight)))
 
-;; Highlighting Code
-(defun highlight-context-line-set-extent-face (extent face)
-  (highlight-context-line-set-extent-property extent 'face face))
 
-(defun highlight-context-line-do-highlight (direct)
-  "Do the actual highlighting.
-If DIRECT is positive we have been called from scroll-up, if negative
-from scroll down."
-  (highlight-context-line-unhighlight)
+(defun highlight-context-get-scroll-direction ()
+  "Returns 1 for scroll up, -1 for scroll down and nil if not
+scrolling at all."
+  (cond
+   ((eq this-command 'scroll-up-command) 1)
+   ((eq this-command 'scroll-down-command) -1)))
+
+(defun highlight-context-line-highlight* (direction)
+  "Find the line to highlight and move the overlay."
   (save-excursion
-    (if (> direct 0)
-        (move-to-window-line next-screen-context-lines)
-      (move-to-window-line
-       (- (highlight-context-line-height) next-screen-context-lines)))
-    (setq highlight-context-line-extent
-          (highlight-context-line-make-extent
-           (point-at-bol)
-           (if (not (eq (point-at-bol) (point-at-eol)))
-               (point-at-eol)
-             (forward-line (- direct))
-             (point-at-eol))))
-    (set-face-background highlight-context-line-face
-                         highlight-context-line-background)
-    (highlight-context-line-set-extent-face
-     highlight-context-line-extent highlight-context-line-face)
-    ;; prepare clean up
-    (add-hook 'pre-command-hook
-              'highlight-context-line-unhighlight)))
+    ;; Jump to the line we want to highlight
+    (move-to-window-line (* direction
+                            next-screen-context-lines))
+    ;; Overlay this line
+    (let ((beg (line-beginning-position))
+          ;; why 2 for the next line is beyond me
+          (end (line-beginning-position 2)))
+      (move-overlay highlight-context-line-overlay beg end))))
 
 (defun highlight-context-line-unhighlight ()
-  "Remove highlighting of the context line if any."
-  (if highlight-context-line-extent
-      (progn
-        (highlight-context-line-delete-extent
-         highlight-context-line-extent)
-        (setq highlight-context-line-extent nil)
-        (remove-hook 'pre-command-hook
-                     'highlight-context-line-unhighlight))))
+  "Delete the overlay again."
+  (when highlight-context-line-overlay
+    (delete-overlay highlight-context-line-overlay)))
 
-(defadvice scroll-up (around highlight-context-line-up activate)
-  ad-do-it
-  (if (not (car (ad-get-args 0)))
-      (highlight-context-line-do-highlight 1)))
+(defun highlight-context-line-highlight ()
+  "Highlight the context line after scrolling.
+Context line is the last line that was visible before starting to
+scroll in the respective direction."
+  (ignore-errors 
+      (let ((scroll-direction
+             (highlight-context-get-scroll-direction)))
+        (when scroll-direction
+          ;; it was a scrolling
+          (highlight-context-line-highlight* scroll-direction)))))
 
-(defadvice scroll-down (around highlight-context-line-down activate)
-    ad-do-it
-    (if (not (car (ad-get-args 0)))
-        (highlight-context-line-do-highlight -1)))
 
 (provide 'highlight-context-line)
 ;; highlight-context-line.el ends here
